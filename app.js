4e706399ffa797787ab49fbb05cd49fa30978af8
Spandan More <56129058+spansucksatcode@users.noreply.github.com>
2020-07-09 19:53:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/views'));
 
 app.get("/", function (req, res) {
     res.render("index");
