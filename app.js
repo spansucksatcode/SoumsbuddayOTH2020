bf4ce8f78dff51b9300806a6087fc0be583644ae
Spandan More <56129058+spansucksatcode@users.noreply.github.com>
2020-07-09 15:05:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("index");
+})
+
+app.get("/surprise", function (req, res) {
+    res.render("easteregg");
+})
+
+app.get("/fool", function (req, res) {
+    res.render("spanishinquisition");
+})
+
+app.listen(8080, function (req, res) {
+    console.log("Listening at port 8080.");
+})
